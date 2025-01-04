e2fc7c1d549b506ac1172e3a58f39f8b01c2a1ff
Sara Palacio <saritapz01@hotmail.com>
2025-01-03 22:28:58 -0500

    WIP: Navbar para proyecto. Mobile y desktop

--- src/app/components/NavBar/index.tsx
+++ src/app/components/NavBar/index.tsx
@@ -1,9 +1,21 @@
+import Link from 'next/link';
 import styles from './styles.module.css';
 
 export default function NavBar() {
     return (
         <nav className={styles.navbar}>
             <h1>Alegría Gatuna App</h1>
+            <ul>
+                <li>
+                    <Link href="/">Alegría Gatuna</Link>
+                </li>
+                <li>
+                    <Link href="/">Adoptar</Link>
+                </li>
+                <li>
+                    <Link href="/">Ver Gatos</Link>
+                </li>
+            </ul>
         </nav>
     )
 }
